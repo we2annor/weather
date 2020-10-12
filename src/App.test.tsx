a28767708e4b1dda3152c713a60c253b03850ddd
ernest annor <we2annor@gmail.com>
2020-10-12 08:06:01 +0100

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import Navigation from "./components/navigation/Navigation";
+//import React from "react";
+import { shallow } from "enzyme";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<Navigation />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//import { mount } from "enzyme";
+//import { render } from "@testing-library/react";
+import App from "./App";
+
+describe("first react component test with enzyme", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
 });
+
+//const wrapper = mount(<App />);
+// test("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText("");
+//   expect(linkElement).toBeInTheDocument();
+// });
